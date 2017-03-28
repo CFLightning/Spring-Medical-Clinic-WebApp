26b20c831820a2f1d91b8eec683d4815c6cb2fb6
Mateusz Antos <cfflightning@gmail.com>
2017-03-28 18:03:59 +0200

    PatientEntity expanded - pesel, email, policy_number added

--- src/main/java/com/pwr/model/PatientEntity.java
+++ src/main/java/com/pwr/model/PatientEntity.java
@@ -15,24 +15,58 @@ public class PatientEntity {
 	private String name;
 	@Column
 	private String surname;
+	@Column
+	private String pesel;
+	@Column
+	private String email;
+	@Column 
+	private String policyNum;
 	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getSurname() {
 		return surname;
 	}
+	
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
-
+	
+	public String getPesel() {
+		return pesel;
+	}
+	
+	public void setPesel(String pesel) {
+		this.pesel = pesel;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPolicyNum() {
+		return policyNum;
+	}
+	
+	public void setPolicyNum(String policyNum) {
+		this.policyNum = policyNum;
+	}
 }
