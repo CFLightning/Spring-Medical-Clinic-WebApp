65c5e4a3167cf13b38f038bbb4a6f5f1bf74b3d7
Mateusz Antos <cfflightning@gmail.com>
2017-05-15 23:53:29 +0200

    HTML template started, component started

--- src/main/client/app/patient-mgmt/patient-creator/patient-creator.component.ts
+++ src/main/client/app/patient-mgmt/patient-creator/patient-creator.component.ts
@@ -1,7 +1,14 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 
 @Component({
   selector: 'patient-creator',
   template: require('./patient-creator.component.html!text'),
-  styles: [require('')]
-})
+  styles: [require('patient-creator.component.css!text')]
+} as Component)
+export class PatientCreatorComponent implements OnInit {
+
+
+  createPatient(): void {
+    
+  }
+}
